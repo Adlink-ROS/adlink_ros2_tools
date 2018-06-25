ed8838498afaf8921ca59c150d287d27405aaf7b
HaoChih Lin <f44006076@gmail.com>
2018-06-25 15:44:22 +0800

    update pong node

--- src/RoundTrip_pong.cpp
+++ src/RoundTrip_pong.cpp
@@ -16,38 +16,70 @@
 #include <iostream>
 #include <memory>
 #include "rclcpp/rclcpp.hpp"
-#include "std_msgs/msg/string.hpp"
+#include "rcutils/cmdline_parser.h" 
+#include "adlink_msgs/msg/ping_pong.hpp"
 
 using std::placeholders::_1;
 
 class RoundTripPONG : public rclcpp::Node
 {
     public:
-    RoundTripPONG() : Node("roundtrip_pong"), count_(0)
+    RoundTripPONG(int argc, char * argv[]) : Node("roundtrip_pong"), count_(0)
     {
-        publisher_ = this->create_publisher<std_msgs::msg::String>("roadtrip_pong", rmw_qos_profile_parameters); //topic, QoS
-        subscription_ = this->create_subscription<std_msgs::msg::String>("roadtrip_ping", 
+        // Verify command arguments            
+        self_id_ = 0;
+        if (rcutils_cli_option_exist(argv, argv + argc, "-i"))
+        {
+            self_id_ = atoi(rcutils_cli_get_option(argv, argv + argc, "-i"));
+            if (self_id_ <= 0) 
+                self_id_ = 0;
+        }
+        
+        publisher_ = this->create_publisher<adlink_msgs::msg::PingPong>("roadtrip_pong", rmw_qos_profile_parameters); //topic, QoS
+        subscription_ = this->create_subscription<adlink_msgs::msg::PingPong>("roadtrip_ping", 
                         std::bind(&RoundTripPONG::topic_callback, this, _1), rmw_qos_profile_parameters);
     }
 
 private:
-    void topic_callback(const std_msgs::msg::String::SharedPtr msg)
+    void topic_callback(const adlink_msgs::msg::PingPong::SharedPtr msg)
     {
-        publisher_->publish(msg);
+        msg_back_ = *msg;
+        msg_back_.sender_id = self_id_;
+        msg_back_.recver_id = msg->sender_id;         
+        publisher_->publish(msg_back_);
         //std::cout << "I heard: " << msg->data.c_str() << ", Id: " << count_ << std::endl; //debug
         count_ ++;
     }
 
-    rclcpp::Publisher<std_msgs::msg::String>::SharedPtr publisher_;
-    rclcpp::Subscription<std_msgs::msg::String>::SharedPtr subscription_;
-    size_t count_;
+    rclcpp::Publisher<adlink_msgs::msg::PingPong>::SharedPtr publisher_;
+    rclcpp::Subscription<adlink_msgs::msg::PingPong>::SharedPtr subscription_;
+    adlink_msgs::msg::PingPong msg_back_;
+    size_t count_, self_id_;
 };
+
+
+void print_usage()
+{
+    printf("ROS2 RoundTrip testing tool:\n");
+    printf("options:\n");
+    printf("-h : Print this help function.\n");
+    printf("-i self_id: Specify the No. of self node. [default=0].\n");
+}
 
 int main(int argc, char * argv[])
 {
     rclcpp::init(argc, argv);
+
+    // Print the help function
+    if (rcutils_cli_option_exist(argv, argv + argc, "-h")) 
+    {
+        print_usage();
+        return 0;
+    }
+
     std::cout << "RoundTrip Testing: pong side !" << std::endl;
-    rclcpp::spin(std::make_shared<RoundTripPONG>());
+    auto pong = std::make_shared<RoundTripPONG>(argc, argv);
+    rclcpp::spin(pong);
     rclcpp::shutdown();
     return 0;
 }
